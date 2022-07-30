db484f5e18ad4137f7cc13506359b3a58f157d87
Hexol <thannigel@gmail.com>
2022-07-30 19:20:37 +0800

    Add function in temperatures.py

--- prac_03/temperatures.py
+++ prac_03/temperatures.py
@@ -8,16 +8,26 @@ F - Convert Fahrenheit to Celsius
 Q - Quit"""
 print(MENU)
 choice = input(">>> ").upper()
+
+
+def convert_to_fahrenheit():
+    return celsius * 9.0 / 5 + 32
+
+
+def convert_to_celsius():
+    return 5 / 9 * (fahrenheit - 32)
+
+
 while choice != "Q":
     if choice == "C":
         celsius = float(input("Celsius: "))
-        fahrenheit = celsius * 9.0 / 5 + 32
+        fahrenheit = convert_to_fahrenheit()
         print("Result: {:.2f} F".format(fahrenheit))
     elif choice == "F":
         # TODO: Write this section to convert F to C and display the result
         # Hint: celsius = 5 / 9 * (fahrenheit - 32)
         fahrenheit = float(input("Fahrenheit: "))
-        celsius = 5 / 9 * (fahrenheit - 32)
+        celsius = convert_to_celsius()
         print("Result: {:.2f} F".format(celsius))
         pass
     else:
@@ -25,3 +35,5 @@ while choice != "Q":
     print(MENU)
     choice = input(">>> ").upper()
 print("Thank you.")
+
+
